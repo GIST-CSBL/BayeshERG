86333243c7faec1c6125a732fa4fe7a1442d6ff9
Hyunho Kim <30457253+sunset222@users.noreply.github.com>
2022-07-09 01:16:46 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo Input CUDA version of your GPU, ex. 10.2
 read cuda_ver
 echo DGL and Pytorch with CUDA v$cuda_ver will be installed. 
 conda install -c dglteam dgl-cuda$cuda_ver=0.4.3 -y
-conda install pytorch cudatoolkit=$cuda_ver -c pytorch -y
+conda install pytorch=1.10.1 cudatoolkit=$cuda_ver -c pytorch -y
 conda install requests -y
 conda install tqdm -y
 conda install -c conda-forge mdtraj -y
