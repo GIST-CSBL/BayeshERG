69f01d0d207b9a4eb0658c42ce47be1eddcb141b
Hyunho Kim <30457253+sunset222@users.noreply.github.com>
2022-07-26 15:01:50 -0700

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda install -c dglteam dgl-cuda10.2=0.4.3 -y
-conda install pytorch=1.10.1 cudatoolkit=10.2 -c pytorch -y
-conda install requests -y
-conda install tqdm -y
-conda install -c conda-forge mdtraj -y
